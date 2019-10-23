206227597ec77ca687f2c3d8eeecf2ee6db3d794
Server <server.ares@gmail.com>
2019-10-23 20:53:10 +0300

    V 0.9a

--- html/part4PrinterInfo/dist/part4PrinterInfo/main-es5.js
+++ html/part4PrinterInfo/dist/part4PrinterInfo/main-es5.js
@@ -823,8 +823,8 @@
             // The list of file replacements can be found in `angular.json`.
             var environment = {
                 production: false,
-                /*apiURL: 'http://116.203.243.136:5000/api/',*/
-                apiURL: 'http://localhost:5000/api/'
+                apiURL: 'http://116.203.243.136:5000/api/'
+                /*apiURL: 'http://localhost:5000/api/'*/
             };
             /*
              * For easier debugging in development mode, you can import the following file
