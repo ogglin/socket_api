9f27b31f31a39abdbf30bc5e5c42604f5cfd3154
Server <server.ares@gmail.com>
2019-10-26 18:53:46 +0300

    V 0.94 fix

--- dbconnect.js
+++ dbconnect.js
@@ -249,7 +249,7 @@ function addError(init_client_error, device_id, error, callback) {
     var d = new Date();
     var n = d.toJSON();
     qaa = "INSERT INTO rdata.errors VALUES ("+init_client_error+", "+ device_id +", '"+ error +"', '"+n+"');";
-    console.log(qaa)
+    console.log(qaa);
     (async () => {
         const client = await pool.connect();
         try {
