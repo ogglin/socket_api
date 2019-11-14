40cc1d2a137211fcfa0b3d0fe3b975003e1ecdf0
Server <server.ares@gmail.com>
2019-11-14 13:53:16 +0300

    V 0.99.1 - fix

--- server.js
+++ server.js
@@ -114,10 +114,7 @@ io.on('connection', function(socket){
             }
             if (obj['server_init'] === 'getDevices' && !obj['status']) {
                 console.log('get: ', data);
-                clients.forEach(client=>{
-                    socket.clients[client].emit('message', '{"status":' + data + '}');
-                });
-
+                socket.broadcast.emit('message', '{"status":' + data + '}');
                 /*for (var key in clients) {
                     socket.emit('message', '{"status":' + data + '}');
                     //clients[key].send('{"status":' + data + '}');
