c5ca7a8095191aece83211a777c6222ed78abffd
Server <server.ares@gmail.com>
2019-12-05 20:00:36 +0300

    V 1.0.1.5f - fix cert and links

--- html/part4PrinterInfo/src/app/shared/socket/socket.service.ts
+++ html/part4PrinterInfo/src/app/shared/socket/socket.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
-import {Address} from '../../devices/shared/model/address';
-import {Client} from '../../devices/shared/model/client';
-import {Customers} from '../../devices/shared/model/customers';
-import {Info} from '../../devices/shared/model/info';
 import {Event} from './model/event';
 
 import io from 'socket.io-client';
 
-const SERVER_URL_GET = 'https://localhost:8443/get';
-const SERVER_URL_PUT = 'https://localhost:8443/put';
+/*const SERVER_URL_GET = 'https://localhost:8443/get';
+const SERVER_URL_PUT = 'https://localhost:8443/put';*/
 /*const SERVER_URL_GET = 'https://socket.api.part4.info:8443/get';
 const SERVER_URL_PUT = 'https://socket.api.part4.info:8443/put';*/
+const SERVER_URL_GET = 'https://dev.socket.part4.info:8443/get';
+const SERVER_URL_PUT = 'https://dev.socket.part4.info:8443/put';
 
 @Injectable({
   providedIn: 'root'
